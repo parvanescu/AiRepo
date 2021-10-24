8232c93bf5f0b328778f97a6f328e878b1b21365
stefan <stefan@stiudeo.com>
2021-10-24 16:54:08 +0300

    basic backend structure

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from controller.Controller import Controller
+from domain.Payload import Payload
+from repo.Repository import Repository
+from service.Service import Service
+
 if __name__ == '__main__':
-    pass
+    repository = Repository(None)
+    service = Service(repository)
+    controller = Controller(service)
